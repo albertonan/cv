4fc243753d051fb21a93e19eaf2ae70c310d19b8
Nan <albertonan@gmail.com>
2020-01-29 09:52:01 +0100

    First Commit

---
+++ translation.js
@@ -0,0 +1,251 @@
+export default {
+    "navBar":{
+        "commonWords":{
+            "quickAccess":{
+                "es":"Acceso rápido",
+                "en":"Quick access"
+            },
+            "aboutMe":{
+                "es":"Sobre mi",
+                "en":"About me"
+            },
+            "experience":{
+                "es":"Experiencia",
+                "en":"Experience"
+            },
+            "projects":{
+                "es":"Proyectos",
+                "en":"Projects"
+            },
+            "studies":{
+                "es":"Estudios",
+                "en":"Studies"
+            },
+            "downloadCV":{
+                "es":"DescargarCV",
+                "en":"DownloadCV"
+            }
+        }
+    },
+    "about":{
+        "commonWords":{
+            "aboutMe":{
+                "es":"sobre mi",
+                "en":"about me"
+            }
+        },
+        "summary":{
+            "es":"Desarrollador como trabajo y como hobby. Albert Nan comenzó desde pequeño a crear pequeñas aplicaciones en consola para automatizar cálculos necesarios para sus sesiones de juego. Aprender nuevos lenguages, nuevos frameworks u otras formas de hacer lo mismo pero de forma optimizada es un placer y un estilo de vida. Desarrollador que siempre está conectado al mundo, se preocupa por el entorno y nunca le dice que no a un desafío.",
+            "en":"Developer as job and as hobby. Albert Nan started at an early age developing simple apps on console to automates some calculations for his playtime on games. Learning new languages, new frameworks or optimized ways to do the same thing is a pleasure and a way of life. Developer that is always connected to the world, that cares for environment and never says no to a challenge."
+        },
+        "subtitle":{
+            "es":"Desarrollador de Back-end & Front-end",
+            "en":"Back-end & Front-end developer"
+        }
+    },
+    "projects": {
+        "conf":{
+            "backgroundColor":"rgb(170, 184, 169)"
+        },
+        "commonWords": {
+            "projects":{
+                "es":"Proyectos",
+                "en":"Projects"
+            },
+            "summary": {
+                "es": "resumen",
+                "en": "summary"
+            },
+            "experienceGained": {
+                "es": "experiencia adquirida",
+                "en": "experience gained"
+            },
+            "media":{
+                "es":"media",
+                "en":"media"
+            }
+        },
+        "list": [
+            {
+                "title": {
+                    "es": "herramienta de colaboración",
+                    "en": "collaboration tool"
+                },
+                "summary": {
+                    "es": "Aplicación web desarrollada a medida con PolymerJS 3.0. Funcionalidades parecidas a Microsoft Teams como chat en tiempo real, llamadas, videollamadas, intercambio de archivos, vista previa de links de Youtube, etc.. Uso de Firebase como base de datos en tiempo real y para autenticación unificada con web de terceros",
+                    "en": "Custom development of an Web App using PolymerJS 3.0. Functionalities like Microsoft Teams: realtime chat,  calls, videocalls, file interchanges, preview of Youtube links, etc.. Firebase used as real time database and for integrated authentication with third party webpage"
+                },
+                "experienceGained": {
+                    "es": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS",
+                    "en": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS"
+                }
+            },
+            {
+                "title": {
+                    "es": "dashboard de administración",
+                    "en": "admin dashboard"
+                },
+                "summary": {
+                    "es": "Aplicación web desarrollada en VueJS para el front y NodeJS para el back. Creación de APIs para recuperar datos de la base de datos de UCCX. Web en la que se muestran los datos de forma organizada, personalizada y en tiempo real",
+                    "en": "Web App developer with VueJS for the front and with NodeJS for the back. API creation for retrieving data from the database of UCCX. On the web, the data is showed in real time in a personallyzed and organized way"
+                },
+                "experienceGained": {
+                    "es": "- VueJS <br> - NodeJS <br> - Llamadas a UCCX con informix",
+                    "en": "- VueJS <br> - NodeJS <br> - UCCX querys with informix"
+                }
+            },
+            {
+                "title": {
+                    "es": "migración microsoft teams - webex teams ",
+                    "en": "microsoft teams - webex teams migration"
+                },
+                "summary": {
+                    "es": "Interfaz web sencilla integrada con un servidor NodeJS que migra desde Microsoft Teams a Webex Teams toda la estructura de la organización: usuarios, grupos, espacios y administradores",
+                    "en": "Simple web interface integrated with a NodeJS back that migrates from Miscrosoft Teams to Webex Teams the structure of the organization: users, groups, spaces and administrators"
+                },
+                "experienceGained": {
+                    "es": "- Microsoft Graph API <br> - Webex Teams API <br> - NodeJs API Rest <br> - HTML, CSS",
+                    "en": "- Microsoft Graph API <br> - Webex Teams API <br> - NodeJs API Rest <br> - HTML, CSS"
+                }
+            },
+            {
+                "title": {
+                    "es": "chatbot",
+                    "en": "chatbot"
+                },
+                "summary": {
+                    "es": "Integración de chatbot con cualquier herramienta de colaboración que ofrezca APIs para leer en tiempo real y responder. Creación de chatbot con lenguaje natural usando Dialogflow",
+                    "en": "Integration of chatbot with any collaboration tool that offers real-time message updates and answering APIs. Creation of chatbot with natural language understanding using Dialogflow"
+                },
+                "experienceGained": {
+                    "es": "- Dialogflow <br> - Rest APIs <br> - NodeJS",
+                    "en": "- Dialogflow <br> - Rest APIs <br> - NodeJS"
+                }
+            },
+            {
+                "title": {
+                    "es": "desarrollo control llamadas cisco call manager",
+                    "en": "call controll development cisco call manager"
+                },
+                "summary": {
+                    "es": "Desarrollo de aplicación web de gestión de llamadas integrada con Cisco Call Manager usando Java y AXL. Control de la telefonía: bloqueo y desbloqueo de terminales, gestión de listas blancas y listas negras de números, posibilidad de programar bloqueos y desbloqueos según semana, día, hora y minutos, gestión de usuarios administradores",
+                    "en": "Web App development for managing calls integrated with Cisco Call Manager using AXL and Java. Controll of the telephony: locking and unlocking, management of white list and black list of numbers, possibility of scheduling locks and unlocks by week, day, hour and minute, management of administrator users"
+                },
+                "experienceGained": {
+                    "es": "- JTAPI <br> - AXL <br> - Java <br> - Apache Tomcat <br> - MySQL",
+                    "en": "- JTAPI <br> - AXL <br> - Java <br> - Apache Tomcat <br> - MySQL"
+                }
+            }
+        ]
+    },
+    "experience": {
+        "conf":{
+            "backgroundColor":"rgb(167, 167, 180)"
+        },
+        "commonWords": {
+            "experience":{
+                "es":"Experiencia",
+                "en":"Experience"
+            },
+            "summary": {
+                "es": "resumen",
+                "en": "summary"
+            },
+            "experienceGained": {
+                "es": "experiencia adquirida",
+                "en": "experience gained"
+            },
+            "media":{
+                "es":"media",
+                "en":"media"
+            }
+        },
+        "list": [
+            {
+                "title": {
+                    "es": "Makenai",
+                    "en": "Makenai"
+                },
+                "summary": {
+                    "es": "Aplicación web desarrollada a medida con PolymerJS 3.0. Funcionalidades parecidas a Microsoft Teams como chat en tiempo real, llamadas, videollamadas, intercambio de archivos, vista previa de links de Youtube, etc.. Uso de Firebase como base de datos en tiempo real y para autenticación unificada con web de terceros",
+                    "en": "Custom development of an Web App using PolymerJS 3.0. Functionalities like Microsoft Teams: realtime chat,  calls, videocalls, file interchanges, preview of Youtube links, etc.. Firebase used as real time database and for integrated authentication with third party webpage"
+                },
+                "experienceGained": {
+                    "es": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS",
+                    "en": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS"
+                },
+                "date":{
+                    "start":{
+                        "es":"18 Marzo 2019",
+                        "en":"18th March 2019"
+                    },
+                    "end":{
+                        "es":"Actualidad",
+                        "en":"Current date"
+                    }
+                },
+                "jobTitle":{
+                    "es":"Desarrollador del Software",
+                    "en":"Software Developer"
+                }
+            }
+        ]
+    },
+    "studies": {
+        "conf":{
+            "backgroundColor":"rgb(214, 204, 188)"
+        },
+        "commonWords": {
+            "studies":{
+                "es":"Estudios",
+                "en":"Studies"
+            },
+            "experience":{
+                "es":"Experiencia",
+                "en":"Experience"
+            },
+            "summary": {
+                "es": "resumen",
+                "en": "summary"
+            },
+            "experienceGained": {
+                "es": "experiencia adquirida",
+                "en": "experience gained"
+            },
+            "media":{
+                "es":"media",
+                "en":"media"
+            }
+        },
+        "list": [
+            {
+                "title": {
+                    "es": "Desarrollo de Aplicaciones Multiplataforma",
+                    "en": "Multiplatform Application Development"
+                },
+                "summary": {
+                    "es": "Aplicación web desarrollada a medida con PolymerJS 3.0. Funcionalidades parecidas a Microsoft Teams como chat en tiempo real, llamadas, videollamadas, intercambio de archivos, vista previa de links de Youtube, etc.. Uso de Firebase como base de datos en tiempo real y para autenticación unificada con web de terceros",
+                    "en": "Custom development of an Web App using PolymerJS 3.0. Functionalities like Microsoft Teams: realtime chat,  calls, videocalls, file interchanges, preview of Youtube links, etc.. Firebase used as real time database and for integrated authentication with third party webpage"
+                },
+                "experienceGained": {
+                    "es": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS",
+                    "en": "- Firebase <br> - PolymerJS <br> - Google Auth <br> - Rest APIs NodeJS"
+                },
+                "date":{
+                    "start":{
+                        "es":"20 Septiembre 2017",
+                        "en":"20th September 2017"
+                    },
+                    "end":{
+                        "es":"30 Junio 2019",
+                        "en":"30th June 2019"
+                    }
+                },
+                "jobTitle":{
+                    "es":"IES Virgen de la Paloma",
+                    "en":"IES Virgen de la Paloma"
+                }
+            }
+        ]
+    }
+}
